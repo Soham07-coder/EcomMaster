b576e65135912574d05e89b27560c632b1353ea8
soham <soham.gore@somaiya.edu>
2024-10-28 20:40:49 +0530

    New

--- lib/screens/product/views/product_buy_now_screen.dart
+++ lib/screens/product/views/product_buy_now_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../../constants.dart';
 import '../../../route/route_constants.dart';
+import 'added_to_cart_message_screen.dart';
 import 'components/product_quantity.dart';
 import 'components/selected_colors.dart';
 import 'components/selected_size.dart';
@@ -253,70 +254,3 @@ class _ProductBuyNowScreenState extends State<ProductBuyNowScreen> {
   }
 }
 
-class AddedToCartMessageScreen extends StatelessWidget {
-  final List<Map<String, dynamic>> items; // Accept the list of items
-
-  const AddedToCartMessageScreen({super.key, required this.items}); // Accept items through constructor
-
-  Future<void> _addItemsToCart() async {
-    // Get a reference to the Firestore collection
-    final cartCollection = FirebaseFirestore.instance.collection('carts');
-
-    // Iterate through the items and add each one to Firestore
-    for (var item in items) {
-      await cartCollection.add(item);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Call the method to add items to Firestore when the screen is built
-    _addItemsToCart();
-
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-          child: Column(
-            children: [
-              const Spacer(),
-              Image.asset(
-                Theme.of(context).brightness == Brightness.light
-                    ? "assets/Illustration/success.png"
-                    : "assets/Illustration/success_dark.png",
-                height: MediaQuery.of(context).size.height * 0.3,
-              ),
-              const Spacer(flex: 2),
-              Text(
-                "Added to cart",
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(height: defaultPadding / 2),
-              const Text(
-                "Click the checkout button to complete the purchase process.",
-                textAlign: TextAlign.center,
-              ),
-              const Spacer(flex: 2),
-              OutlinedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, entryPointScreenRoute);
-                },
-                child: const Text("Continue Shopping"),
-              ),
-              const SizedBox(height: defaultPadding),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, cartScreenRoute);
-                },
-                child: const Text("Checkout"),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
